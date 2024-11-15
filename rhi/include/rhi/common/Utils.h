b07441c06f3eea2c712c41fba4c6f2a7b46f7b6f
Hongliang_Jia <Hongliang_Jia@asus.com>
2024-11-15 16:21:17 +0800

    move structs to Independent header

--- rhi/include/rhi/common/Utils.h
+++ rhi/include/rhi/common/Utils.h
@@ -53,7 +53,7 @@ namespace rhi
     inline constexpr T operator ~ (T a) { return T(~uint32_t(a)); } /* NOLINT(bugprone-macro-parentheses) */ \
     inline constexpr bool operator !(T a) { return uint32_t(a) == 0; } \
     inline constexpr bool operator ==(T a, uint32_t b) { return uint32_t(a) == b; } \
-    inline constexpr bool operator !=(T a, uint32_t b) { return uint32_t(a) != b; }
+    inline constexpr bool operator !=(T a, uint32_t b) { return uint32_t(a) != b; }	\
 
 
 	template <typename T>
